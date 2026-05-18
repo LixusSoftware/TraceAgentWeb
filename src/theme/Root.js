0d3d2f3f40939b513ae4143e819588c3e72e1ebc
Javi-RC <javirodriguezcastellano@gmail.com>
2026-05-18 11:52:23 +0200

    Add JSON-LD structured data and canonical links in Root

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -2,10 +2,51 @@ import React from 'react';
 import Root from '@theme-original/Root';
 import CommandPalette from '@site/src/components/CommandPalette';
 import FaviconManager from '@site/src/components/FaviconManager';
+import Head from '@docusaurus/Head';
+import {useLocation} from '@docusaurus/router';
 
 export default function RootWrapper(props) {
+  const {pathname} = useLocation();
+  const siteUrl = 'https://traceagent.vercel.app';
+  const canonical = `${siteUrl}${pathname}`;
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Organization",
+        "@id": `${siteUrl}/#organization`,
+        "name": "TraceAgent",
+        "url": siteUrl,
+        "logo": `${siteUrl}/img/logo.png",
+        "sameAs": [
+          "https://github.com/LixusSoftware"
+        ]
+      },
+      {
+        "@type": "WebSite",
+        "@id": `${siteUrl}/#website`,
+        "url": siteUrl,
+        "name": "TraceAgent",
+        "publisher": {"@id": `${siteUrl}/#organization`}
+      },
+      {
+        "@type": "WebPage",
+        "@id": `${canonical}#webpage`,
+        "url": canonical,
+        "inLanguage": "en",
+        "name": "TraceAgent",
+        "isPartOf": {"@id": `${siteUrl}/#website`}
+      }
+    ]
+  };
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonical} />
+        <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
+      </Head>
       <Root {...props} />
       <CommandPalette />
       <FaviconManager />
